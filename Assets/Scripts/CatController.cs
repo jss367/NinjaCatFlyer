b0890c3c612b6bd839448b29a60a29be1254fe0b
jss367 <julius.simonelli@gmail.com>
2014-12-27 20:12:51 -0500

    First version
    test with new name!

---
+++ Assets/Scripts/CatController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatController : MonoBehaviour {
+	public float jetpackForce = 75.0f;
+	public float forwardMovementSpeed = 3.0f;
+
+	public Transform groundCheckTransform;
+	private bool grounded;
+	public LayerMask groundCheckLayerMask;
+	Animator animator;
+
+	public ParticleSystem jetpack;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		bool jetpackActive = Input.GetButton ("Fire1");
+
+		if (jetpackActive) {
+						rigidbody2D.AddForce (new Vector2 (0, jetpackForce));
+				}
+		Vector2 newVelocity = rigidbody2D.velocity;
+		newVelocity.x = forwardMovementSpeed;
+		rigidbody2D.velocity = newVelocity;
+
+		UpdateGroundedStatus ();
+
+		AdjustJetpack (jetpackActive);
+	}
+
+	void UpdateGroundedStatus()
+	{
+				grounded = Physics2D.OverlapCircle (groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+
+				animator.SetBool ("grounded", grounded);
+		}
+
+	void AdjustJetpack(bool jetpackActive)
+	{
+				jetpack.enableEmission = !grounded;
+				jetpack.emissionRate = jetpackActive ? 300.0f : 75.0f;
+		}
+}
---
+++ Assets/Scripts/GeneratorScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GeneratorScript : MonoBehaviour {
+	public GameObject[] availableMountains;
+	public List<GameObject> currentMountains;
+	private float screenWidthInPoints;
+
+	// Use this for initialization
+	void Start () {
+		float height = 2.0f * Camera.main.orthographicSize;
+		screenWidthInPoints = height * Camera.main.aspect;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate()
+	{
+		GenerateMountainIfRequired ();
+	}
+
+	void AddMountains(float farthestMountainEndX)
+	{
+		int randomIndex = Random.Range (0, availableMountains.Length);
+		GameObject mt = (GameObject)Instantiate (availableMountains [randomIndex]);
+		float mtWidth = mt.transform.FindChild ("Floor").localScale.x;
+		float mtCenter = farthestMountainEndX + mtWidth * 0.275f;
+		mt.transform.position = new Vector3 (mtCenter, 0, 0);
+		currentMountains.Add (mt);
+}
+
+	void GenerateMountainIfRequired()
+	{
+ 		List<GameObject> mountainsToRemove = new List<GameObject>();
+		bool addMountains = true;
+		float playerX = transform.position.x;
+		float removeMountainX = playerX - screenWidthInPoints;
+		float addMountainX = playerX + screenWidthInPoints;
+		float farthestMountainEndX = 0;
+
+		foreach (var mt in currentMountains)
+		{
+			float mtWidth = mt.transform.FindChild("Floor").localScale.x;
+			float mtStartX = mt.transform.position.x - (mtWidth * 0.5F);
+			float mtEndX = mtStartX + mtWidth;
+
+			if (mtStartX > addMountainX)
+				addMountains = false;
+
+			if (mtEndX < removeMountainX)
+				mountainsToRemove.Add(mt);
+
+			farthestMountainEndX = Mathf.Max(farthestMountainEndX, mtEndX);
+	}
+		foreach (var mt in mountainsToRemove)
+		{
+			currentMountains.Remove(mt);
+			Destroy(mt);
+		}
+
+		if (addMountains)
+			AddMountains(farthestMountainEndX);
+	}
+
+}
